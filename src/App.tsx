fc1f35158aa27b0822cc1693f194fd497f9a3aa6
ivykxzhang <ivykxzhang@tencent.com>
2024-09-26 15:15:40 +0800

    feat:封装组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,11 @@
-import { useEffect, useState } from 'react';
 import './App.less';
-import { getUsers } from './api/users';
-import { Select, message } from 'antd';
+import UserSelect from './components/UserSelect';
 
 function App() {
-  const [users, setUsers] = useState<{ label: string; value: string }[]>([]);
-  const [user, setUser] = useState<string>('');
-  const [searchValue, setSearchValue] = useState<string>('');
-
-  const getUserList = async (keyword: string) => {
-    const res = await getUsers(keyword);
-    const {
-      data: { Response },
-    } = res;
-    if (Response.Data) {
-      setUsers(
-        Response.Data.map(item => ({
-          label: `姓名:${item.UserName}, 年龄:${item.UserAge}`,
-          value: item.UserName,
-        }))
-      );
-      return;
-    }
-    if (Response.Error) {
-      message.error(`${Response.Error.Code}, ${Response.Error.Message}`);
-    }
-  };
 
   return (
     <>
-      <Select
-        options={users}
-        value={user}
-        onChange={setUser}
-        showSearch
-        searchValue={searchValue}
-        onSearch={value => {
-          setSearchValue(value);
-          getUserList(value);
-        }}
-        filterOption={false}
-        style={{width: 200}}
-        allowClear
-      ></Select>
+      <UserSelect></UserSelect>
     </>
   );
 }
---
+++ src/components/UserSelect.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { getUsers } from '../api/users';
+import { Select, message } from 'antd';
+
+const UserSelect: React.FC = () => {
+  const [users, setUsers] = useState<{ label: string; value: string }[]>([]);
+  const [user, setUser] = useState<string>('');
+  const [searchValue, setSearchValue] = useState<string>('');
+
+  const getUserList = async (keyword: string) => {
+    const res = await getUsers(keyword);
+    const {
+      data: { Response },
+    } = res;
+    if (Response.Data) {
+      setUsers(
+        Response.Data.map(item => ({
+          label: `姓名:${item.UserName}, 年龄:${item.UserAge}`,
+          value: item.UserName,
+        }))
+      );
+      return;
+    }
+    if (Response.Error) {
+      message.error(`${Response.Error.Code}, ${Response.Error.Message}`);
+    }
+  };
+
+  return (
+    <Select
+      options={users}
+      value={user}
+      onChange={setUser}
+      showSearch
+      searchValue={searchValue}
+      onSearch={value => {
+        setSearchValue(value);
+        getUserList(value);
+      }}
+      filterOption={false}
+      style={{ width: 200 }}
+      allowClear
+    ></Select>
+  );
+};
+
+export default UserSelect;
